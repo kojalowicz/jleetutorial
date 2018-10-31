e53b23b76947301f2d35cc5f2c4ca487deb72243
kojalowicz <piotr.kojalowicz@gmail.com>
2018-10-31 14:04:43 +0100

    Fixing bugs for checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  
+/**
+ * It is greetign someone
+ */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
